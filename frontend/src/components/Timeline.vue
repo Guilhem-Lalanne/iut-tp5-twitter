57d2ee038e3f4000d1652b827b63455284147d48
LALANNE GUILHEM p0606160 <p0606160@ucbl.fr>
2017-06-23 09:40:48 +0200

    Created file Timeline with list of fake tweets (json).

---
+++ frontend/src/components/Timeline.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="tweet in tweets">
+        {{ tweet }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      tweets: ['tweet 1', 'tweet 2', 'tweet 3']
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: block;
+    margin: 0 10px;
+  }
+</style>
