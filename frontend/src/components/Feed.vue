256068ac5abf066a1a02d4ee3da4d39edd4a1897
LALANNE GUILHEM p0606160 <p0606160@ucbl.fr>
2017-06-23 13:13:00 +0200

    New file Feed with list of tweets from Timeline.

---
+++ frontend/src/components/Feed.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="feed">
+    <ul>
+      <li v-for="tweet in tweets"><tweet :tweet="tweet"/></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Tweet from './Tweet'
+export default {
+  name: 'feed',
+  components: {Tweet},
+  props: ['tweets']
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+ list-style-type: none;
+ padding: 0;
+}
+li {
+ display: block;
+ margin: 0 10px;
+}
+</style>
